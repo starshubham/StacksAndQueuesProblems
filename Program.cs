36d4c1396c0308ded5d6a42ef68d6dd9f6be8844
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-27 14:53:52 +0530

    Added Program.cs file

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StackAndQueueUsingLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1
+            LinkedListStack Stack = new LinkedListStack();
+            Stack.push(70);
+            Stack.push(30);
+            Stack.push(56);
+            Stack.Display();
+            Console.WriteLine("*********Program End*********\n");
+
+            //UC2
+            //Stack.Peek();
+            //Stack.Pop();
+            Stack.isEmpty();
+            Stack.Display();
+            Console.WriteLine("*********Program End*********\n");
+
+            //UC3
+            LinkedListQueue Queue = new LinkedListQueue();
+            Queue.Enqueue(56);
+            Queue.Enqueue(30);
+            Queue.Enqueue(70);
+            Queue.Display();
+            Console.WriteLine("*********Program End*********\n");
+
+            //UC4
+            //Queue.Dequeue();
+            Queue.isEmpty();
+            Queue.Display();
+
+        }
+    }
+}
