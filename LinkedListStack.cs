0f56f21011e1343397f1ae9e2f9f36dd76e8c7fc
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-27 14:57:00 +0530

    Added UC1 and UC2 problem

---
+++ LinkedListStack.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+/*
+UC1- Ability to create a Stack of 56->30->70
+- Use LinkedList to do the Stack Operations
+- Here push will internally call add method on LinkedList.
+- So 70 will be added first then 30 and then 56 to make 56 on top of the Stack
+
+UC2- Ability to peek and pop from the Stack till it is empty 56->30->70
+- Use LinkedList to do the Stack Operations
+ */
+
+namespace StackAndQueueUsingLinkedList
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+    public class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        internal void push (int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is pushed to the stack", value);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty.");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack", this.top.data);
+        }
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is Empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0}", this.top.data);
+            this.top = this.top.next;
+        }
+        internal void isEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+        }
+    }
+}
